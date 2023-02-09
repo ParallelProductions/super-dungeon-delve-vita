ec5d4a5cc6b64aef3617c368fb8671eb18bc07f4
ParallelProductions <42305632+ParallelProductions@users.noreply.github.com>
2023-02-09 18:08:50 -0500

    Removed Original Files

--- core/game-over.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _ready():
-	$ScoresLabel.text = "Depth:  " + str(globals.depth * 100) + "ft\nGold:  " + str(globals.gold) + "\nKills:  " + str(globals.kills)
-	
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_cancel"):
-		get_tree().change_scene("res://core/start-menu.tscn")
